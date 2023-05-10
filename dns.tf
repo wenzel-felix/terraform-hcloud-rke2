f62e251849e889543a24cbacea65a7ecb04b0013
Felix <felixwenzel0@gmail.com>
2023-05-10 22:55:02 +0200

    fixed DNS setting on deployment

--- dns.tf
+++ dns.tf
@@ -1,5 +1,9 @@
+locals {
+  ip_addresses = concat(hcloud_server.master[*].ipv4_address, hcloud_server.worker[*].ipv4_address)
+}
+
 resource "cloudflare_record" "wildcard" {
-  for_each = var.create_cloudflare_dns_record ? toset(concat(hcloud_server.master[*].ipv4_address, hcloud_server.worker[*].ipv4_address)) : []
+  for_each = var.create_cloudflare_dns_record ? {for id in range(var.master_node_count + var.worker_node_count): id => local.ip_addresses[id]} : {}
   zone_id  = var.cloudflare_zone_id
   name     = "*.${var.cloudflare_domain}"
   type     = "A"
